849893f0ab7d94a6a7a73882fde8318eef5eb365
sohyun <rmsidthgusdl81@gmail.com>
2024-04-04 06:00:45 +0900

    FE

--- Problem5.js
+++ Problem5.js
@@ -4,3 +4,14 @@ let user = {
   name: "예린",
   part: "FE",
 };
+
+const json = JSON.stringify(user);
+console.log(typeof json);
+console.log(json);
+
+//const json1 = JSON.stringify(json);
+
+//const json2 = JSON.parse(json);
+const json2 = JSON.parse(json);
+console.log(typeof json2);
+console.log(json2);
--- Problem6.js
+++ Problem6.js
@@ -1,6 +1,18 @@
 // 6번 문제
 
-const arr = [{ part: "fe", name: "홍현", age: 26},
-  { part: "fe", name: "애리", age: 24},
-  { part: "fe", name: "나연", age: 23},
-  { part: "staff", name: "나현", age: 24}];
+const arr = [{ part: "fe", name: "새연", age: 22},
+{ part: "fe", name: "명묵", age: 25},
+{ part: "fe", name: "지후", age: 23},
+{ part: "staff", name: "나현", age: 24}];
+
+  //staff만 배열에서 지우기
+  let FE = arr.filter((arr) => arr.part !== "staff")
+  console.log(FE);
+  
+  //age 순으로 내림차순 정렬
+  let ages = FE.sort((a,b) => b.age - a.age);
+  console.log(ages); 
+
+  //part를 key로 가지는 vaule들만 대문자로 
+  let upper = ages.map(item => ({ ...item, part: item.part.toUpperCase()}));
+  console.log(upper);
